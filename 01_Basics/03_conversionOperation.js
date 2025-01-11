0e3965c8dbdf6f901a7fa5fb8d0f94a1466431cf
Ayush-Tolani <126640786+Ayush-Tolani@users.noreply.github.com>
2025-01-11 12:17:23 +0000

    DOM

---
+++ 01_Basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = true
+
+// console.log(typeof score);
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+
+let isLoggedIn="rk"
+let booleanisLoggedIn=Boolean(isLoggedIn)
+// console.log(booleanisLoggedIn);
+
+
+//1=>true;0=>false;
+//""=>false;"rk"=>true
+
+// ***************operations************
+
+let value=3
+let negValue=-value
+// console.log(negValue);
+
+let str1="radha"
+let str2=" krishna"
+let str3=str1+str2
+console.log(str3);
+
